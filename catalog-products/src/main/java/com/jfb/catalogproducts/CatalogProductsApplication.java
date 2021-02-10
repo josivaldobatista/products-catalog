f06e7904c5373dbfd6630b456cbe15ec18dcf3ec
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-02-10 14:01:58 -0300

    First commit, Project created.

---
+++ catalog-products/src/main/java/com/jfb/catalogproducts/CatalogProductsApplication.java
@@ -0,0 +1,13 @@
+package com.jfb.catalogproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogProductsApplication.class, args);
+	}
+
+}
---
+++ catalog-products/src/test/java/com/jfb/catalogproducts/CatalogProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfb.catalogproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
