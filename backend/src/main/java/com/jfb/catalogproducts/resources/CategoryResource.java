62ec9a5aaac7b17e0b7d1745d5adc4c8aca12e3c
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-02-10 19:05:08 -0300

    DTO to Category.

--- backend/src/main/java/com/jfb/catalogproducts/resources/CategoryResource.java
+++ backend/src/main/java/com/jfb/catalogproducts/resources/CategoryResource.java
@@ -2,7 +2,7 @@ package com.jfb.catalogproducts.resources;
 
 import java.util.List;
 
-import com.jfb.catalogproducts.entities.Category;
+import com.jfb.catalogproducts.dto.CategoryDTO;
 import com.jfb.catalogproducts.services.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +19,8 @@ public class CategoryResource {
   private CategoryService service;
 
   @GetMapping
-  public ResponseEntity<List<Category>> findAll() {
-    List<Category> list = service.findAll();
+  public ResponseEntity<List<CategoryDTO>> findAll() {
+    List<CategoryDTO> list = service.findAll();
     return ResponseEntity.ok().body(list);
   }
 
--- backend/src/main/java/com/jfb/catalogproducts/services/CategoryService.java
+++ backend/src/main/java/com/jfb/catalogproducts/services/CategoryService.java
@@ -1,7 +1,9 @@
 package com.jfb.catalogproducts.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.jfb.catalogproducts.dto.CategoryDTO;
 import com.jfb.catalogproducts.entities.Category;
 import com.jfb.catalogproducts.repositories.CategoryRepository;
 
@@ -14,9 +16,10 @@ public class CategoryService {
 
   @Autowired
   private CategoryRepository repository;
-  
+
   @Transactional(readOnly = true)
-  public List<Category> findAll() {
-    return repository.findAll();
+  public List<CategoryDTO> findAll() {
+    List<Category> list = repository.findAll();
+    return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
   }
 }
