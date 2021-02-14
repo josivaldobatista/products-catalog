eabccb6e47e642c7b5062e4e4d1fed22192e64e2
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-02-14 13:52:11 -0300

    Product entity sql script.

--- backend/src/main/java/com/jfb/productscatalog/entities/Category.java
+++ backend/src/main/java/com/jfb/productscatalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.jfb.productscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -29,6 +32,9 @@ public class Category implements Serializable {
 
   @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant updatedAt;
+
+  @ManyToMany(mappedBy = "categories")
+  private Set<Product> products = new HashSet<>();
 
   public Category() {
   }
@@ -70,6 +76,14 @@ public class Category implements Serializable {
   @PreUpdate
   public void PreUpdate() {
     updatedAt = Instant.now();
+  }
+
+  public Set<Product> getProducts() {
+    return this.products;
+  }
+
+  public void setProducts(Set<Product> products) {
+    this.products = products;
   }
 
   @Override
