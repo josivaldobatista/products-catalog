1e6fa6db889c28bafdf30148cd501e92e8bc24f5
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-02-24 23:08:15 -0300

    Implementing Oauth2 authorization server.

--- backend/src/main/java/com/jfb/productscatalog/services/UserService.java
+++ backend/src/main/java/com/jfb/productscatalog/services/UserService.java
@@ -15,17 +15,25 @@ import com.jfb.productscatalog.repositories.UserRepository;
 import com.jfb.productscatalog.services.exceptions.DatabaseException;
 import com.jfb.productscatalog.services.exceptions.ResourceNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+  private static  Logger logger = LoggerFactory.getLogger(UserService.class);
 
   @Autowired
   private BCryptPasswordEncoder passwordEncoder;
@@ -90,6 +98,17 @@ public class UserService {
       Role role = roleRepository.getOne(roleDto.getId());
       entity.getRoles().add(role);
     }
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = repository.findByEmail(username);
+    if (user == null) {
+      logger.error("Email não encontrado: " + username);
+      throw new UsernameNotFoundException("Email não encontrado");
+    }
+    logger.info("Email encontrado: " + username);
+    return user;
   }
 
 }
