4b722d9aa89a35e2d21bfb8a143a4eb71e195a69
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-02-26 00:49:36 -0300

    H2 security configuration.

--- backend/src/main/java/com/jfb/productscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/jfb/productscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.jfb.productscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,7 +16,10 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-  private static final String[] PUBLIC = { "/oauth/tokem" };
+  @Autowired
+  private Environment env;
+
+  private static final String[] PUBLIC = { "/oauth/tokem", "/h2-console/**" };
 
   private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
@@ -35,6 +41,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
   // Abaixo a Configuração das rotas.
   @Override
   public void configure(HttpSecurity http) throws Exception {
+    
+    // Liberar o H2 no perfil de test.
+    if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+      http.headers().frameOptions().disable();
+    }
+
     http.authorizeRequests()
         .antMatchers(PUBLIC).permitAll()
         .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
