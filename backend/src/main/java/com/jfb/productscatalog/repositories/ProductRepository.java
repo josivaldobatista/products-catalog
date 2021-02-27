1198f99d56df755bfcc0cdf4b7da26a94d27eae8
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-02-26 21:58:56 -0300

    Problem n + 1 Products and Categories fixed.

--- backend/src/main/java/com/jfb/productscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/jfb/productscatalog/repositories/ProductRepository.java
@@ -1,11 +1,18 @@
 package com.jfb.productscatalog.repositories;
 
+import java.util.List;
+
 import com.jfb.productscatalog.entities.Product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+  // Resolvendo o problema n + 1 com a query abaixo de Products e Category.
+  @Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products")
+  List<Product> findProductsCategories(List<Product> products);
 
 }
